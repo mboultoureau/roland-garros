c0a385ad10760216800ade9de724447df80453aa
mboultoureau <contact@mboultoureau.com>
2022-10-27 15:48:09 +0200

    Adding CORS policy

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,5 +3,10 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import axios, { api } from './boot/axios';
 
+  onMounted(async () => {
+    await api.get('/welcome')
+  })
 </script>

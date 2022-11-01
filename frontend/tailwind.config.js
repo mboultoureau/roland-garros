4a4e524b7f59006a43a1704c19b03fe5b23b2c73
paulbreton <paul.breton@cooperl.com>
2022-11-01 12:09:40 +0100

    feat(test): add test E2E

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/*/*.{html,js,vue}',
+    './src/pages/**/*.{html,js,vue}',
     './src/components/*.{html,js,vue}',
     './src/components/*/*.{html,js,vue}'
   ],

dbb8c978d2a20b616fef0877bcee885cc48b619a
paulbreton <paul.breton@cooperl.com>
2022-11-21 21:00:54 +0100

    fix navigation reload

--- frontend/src/boot/navigation-gards.ts
+++ frontend/src/boot/navigation-gards.ts
@@ -1,13 +1,18 @@
 import { useAuthStore } from 'src/stores/auth';
 import { useUserStore } from 'src/stores/user';
 import { AuthentificationError } from 'src/helpers/notify';
-import { Roles } from 'src/models/user';
+import { Roles, User } from 'src/models/user';
+import { LocalStorage } from 'quasar';
 
 export default ({ router }) => {
   const authStore = useAuthStore();
   const userStore = useUserStore();
+
+  if (LocalStorage.has('user')) {
+    userStore.setUser(LocalStorage.getItem('user') as User);
+  }
   // Avant de rediriger le visiteur
-  router.beforeEach((to, from, next) => {
+  router.beforeResolve((to, from, next) => {
     if (to.matched.some((record) => record.meta.requireAuth)) {
       if (authStore.getToken) {
         if (
--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -30,20 +30,15 @@
 <script setup lang="ts">
 import { LocalStorage } from 'quasar';
 import Loader from 'src/components/shared/Loader.vue'
-import { User } from 'src/models/user';
 import { useCountryStore } from 'src/stores/country';
-import { useUserStore } from 'src/stores/user';
 import { onMounted, ref } from 'vue';
 
 const loaderPage = ref(false)
 const countryStore = useCountryStore()
-const userStore = useUserStore()
 
 onMounted(async () => {
   loaderPage.value = true
-  if(LocalStorage.has('user')) {
-    userStore.setUser(LocalStorage.getItem('user') as User)
-  }
+  
   //***************************** */
   //TODO: fetch si role === admin
   //await courtStore.fetch() 
